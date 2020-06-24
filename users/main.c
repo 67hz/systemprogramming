3a18f4be24795d86d51d32c6343f48e0a126409c
Aaron Hinojosa <67hz@protonmail.com>
2020-06-24 18:51:16 -0500

    sys info and user

--- users/main.c
+++ users/main.c
@@ -1,9 +1,36 @@
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <pwd.h>
 #include <string.h>
+#include "tlpi_hdr.h"
 #include "ugid_functions.h"
 
+
+/*
+ * use feof to check if EOF on fp
+ * int feof(FILE *stream);
+ */
+
+#define MAX_NAME  100
+
+/**
+ * this seems overkill. is there a better way to avoid white space
+ * in fgets?
+ */
+void
+trimTrailingWhiteSpace(char *str)
+{
+  size_t i = 0;
+  size_t index = -1;
+  while (str[i] != '\0') {
+    if (str[i] != ' ' && str[i] != '\n')
+      index = i;
+
+    i++;
+  }
+  str[index + 1]  = '\0';
+}
 
 int main(int argc, char *argv[])
 {
@@ -11,14 +38,37 @@ int main(int argc, char *argv[])
   gid_t groupId;
   char *fetchedName;
   char *groupName;
-  const char *name = "aaron";
-  char copiedName[6];
+  char *name;
+  char runName[MAX_NAME];
+
+  if (argc < 2) {
+    FILE *fpipe;
+
+    if ((fpipe = popen("whoami", "r")) == 0)
+      errExit("Could not determine user");
+
+    if (fpipe == NULL)
+      errExit("popen");
+
+    fgets(runName, MAX_NAME, fpipe);
+
+    name = strdup(runName);
+    trimTrailingWhiteSpace(name);
+
+    pclose(fpipe);
+  } else
+    name = argv[1];
 
   userId = userIdFromName(name);
+
+  if (userId == -1)
+    errExit("No user id found");
+
   fetchedName = userNameFromId(userId);
   groupId = groupIdFromName(fetchedName);
   groupName = groupNameFromId(groupId);
 
+  char copiedName[6];
   memcpy(copiedName, fetchedName, 5);
   
   printf("User Id from %s: %d\n", name, userId);
