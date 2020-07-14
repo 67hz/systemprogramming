31fbbc45e518949555ea63510cdb83427ba301fc
Aaron Hinojosa <67hz@protonmail.com>
2020-07-13 19:24:50 -0500

    More process structure

--- sysinfo/process_tree.c
+++ sysinfo/process_tree.c
@@ -13,7 +13,6 @@
  *
  */
 
-
 #include <unistd.h>
 #include <sys/types.h>
 #include <fcntl.h>
@@ -21,11 +20,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-#include <sys/stat.h>   /* for stat macros */
+#include <sys/stat.h>		/* for stat macros */
 #include "error_functions.h"
 #include "tlpi_hdr.h"
 #include "ugid_functions.h"
-
 
 #ifdef ROOT_RUN
 #define PROC_BASE           "proc"
@@ -50,31 +48,123 @@
 typedef struct _proc PROC;
 typedef struct _list_head list_head;
 
-struct _proc
-{
-    pid_t   pid;
-    /* pid_t   pgid; */
-    /* uid_t   uid; */
-    PROC    *parent;
-    list_head *children;
-    char    name[LINE_MAX]; /* _PC_NAME_MAX instead? */
+struct _proc {
+	pid_t pid;
+	/* pid_t   pgid; */
+	/* uid_t   uid; */
+	PROC *parent;
+	list_head *children;
+	char name[LINE_MAX];	/* _PC_NAME_MAX instead? */
 };
 
 struct _list_head {
-    PROC* process;
-    list_head *next;
+	PROC *process;
+	list_head *next;
 };
+
+list_head *
+list_init()
+{
+	list_head *head = malloc(sizeof (*head));
+	if (head == NULL)
+        errExit("failed to init list\n");
+
+	head->next = head;
+
+	return head;
+}
+
+list_head *
+list_add(list_head *list, list_head *node)
+{
+    if (list == NULL) {
+        list = list_init();
+        list->process = node->process;
+        return list;
+    }
+
+	list_head *temp = list->next;
+	node->next = temp;
+	list->next = node;
+    return list;
+}
+
+
+
+void
+print_list(list_head * list)
+{
+	list_head *p, *c;
+    PROC *par;
+	klist_for_each(p, list) {
+		printf("%s: %zu\n", p->process->name, (long) p->process->pid);
+
+
+        /* for (par = p->process->parent; par != NULL; par = par->parent) */
+        /*     printf("->%zu-|%s", (long) p->process->parent->pid, p->process->parent->name); */
+        /* printf("\n"); */
+
+
+        if (p->process->children) {
+            /* print_list(p->process->children); */
+            klist_for_each(c, p->process->children) {
+                printf("|-");
+                printf("-%zu-|\t%s\n", (long) c->process->pid, c->process->name);
+            }
+
+            printf("\n");
+        }
+	}
+}
+
+
+void
+free_list(list_head * list)
+{
+	list_head *p;
+	klist_for_each(p, list) {
+		printf("deleting: %zu %s\n", (long) p->process->pid,
+		       p->process->name);
+
+		if (p != p->next) {
+			list_head *temp = p->next;
+			p->next = p->next->next;
+			free(temp);
+		}
+	}
+}
 
 static Boolean
 str_starts_with(const char *restrict str, const char *restrict prefix)
 {
-    while (*prefix) {
-        if (*prefix++ != *str++)
-            return 0;
-    }
-    return 1;
+	while (*prefix) {
+		if (*prefix++ != *str++)
+			return 0;
+	}
+	return 1;
 }
 
+/**
+ * @param [out] list points to match
+ * @returns TRUE if match else FALSE
+ */
+static Boolean
+list_find_pid_match (list_head **list_out, pid_t pid)
+{
+    list_head *list = *list_out;
+    if (!list || !list->next)
+        return FALSE;
+
+    list_head *p;
+    klist_for_each(p, list) {
+        if (pid_matches(p, pid)) {
+            *list_out = p;
+            return TRUE;
+        }
+    }
+    return FALSE;
+
+}
 
 /**
  * @brief return *CHILD by pid or allocate space for new CHILD
@@ -87,90 +177,96 @@ str_starts_with(const char *restrict str, const char *restrict prefix)
  * @remarks Align memory or allocate enough space for all CHILD's at once?
  */
 list_head *
-proc_fetch_or_alloc(list_head *list, pid_t pid) {
+proc_fetch_or_alloc(list_head *list, pid_t pid)
+{
     list_head *p;
-    PROC *proc;
     klist_for_each(p, list) {
         if (pid_matches(p, pid)) {
-            printf("got match\n");
             return p;
         }
     }
 
-    /*
-     * no match found 
-     * will always have a p - inits to circular list
-     * p = list if not found above
-     */
-    list_head *new_node = malloc(sizeof(*new_node));
-    new_node->process = malloc(sizeof(PROC));
-    if (!new_node || !new_node->process)
-        errExit("Failed to malloc node\n");
+	/* no match found */
+	list_head *new_node = malloc(sizeof (*new_node));
 
+	new_node->process = malloc(sizeof (PROC));
+	if (!new_node || !new_node->process)
+		errExit("Failed to malloc node\n");
 
-    /* insert node into chain */
-    list_head *temp = list->next;
-    new_node->next = temp;
-    list->next = new_node;
-
-
-    return new_node;
+	/* insert node into chain */
+    (void) list_add(list, new_node);
+	return new_node;
 }
 
-static pid_t
-process_fill_by_status (char *buf, list_head *node, pid_t pid)
+list_head *
+list_add_proc(list_head *list, PROC *process)
 {
-    pid_t ppid;
+    list_head *node;
+    if (!process->pid)
+        errExit("No pid available\n");
 
-
-    if (str_starts_with(buf, "Name")) {
-        sscanf(buf, "%*s%s", node->process->name);
-        return 0;
+    if (list_find_pid_match(&list, process->pid)) {
+        return list;
     }
 
-    if (str_starts_with(buf, "Pid")) {
-        sscanf(buf, "%*[^]0-9]%d", &node->process->pid);
-        return 0;
-    }
+    node = malloc(sizeof(*node));
+    node->process = process;
 
+    list = list_add(list, node);
+    return list;
+}
 
-    if (str_starts_with(buf, "PPid")) {
-        sscanf(buf, "%*[^]0-9]%d", &ppid);
-        if (ppid > 0 && ppid != 1)
-            return ppid;
-    }
-
-    return 0;
+static char *
+get_path(pid_t pid)
+{
+    /* @TODO size path */
+	static char path[BUFSIZ];
+	sprintf(path, "%s/%zu/%s", PROC_BASE, (long) pid, PROC_STATUS);
+    return path;
 }
 
 static void
-read_proc_dir_by_pid (pid_t pid, list_head *process)
+read_proc_dir_by_pid(pid_t pid, list_head *list, list_head *child)
 {
-    FILE *file;
-    char *path;
-    char buf[LINE_MAX];
-    int ret_pid;
+	FILE *file;
+	char *path;
+	char buf[LINE_MAX];
+    pid_t ppid;
 
-    if (!(path = malloc(strlen(PROC_BASE) +
-                    strlen(PROC_STATUS) + 10))) /* extra '/'s, pid, and NL */
-        errExit("Could not get full path: %zu", (long) pid);
+    path = get_path(pid);
+    if (!path)
+        return;
 
-    sprintf(path, "%s/%zu/%s", PROC_BASE, (long) pid, PROC_STATUS);
-
-
-    if ((file = fopen (path, "r")) != NULL) {
-        /* printf("path: %s\n", path); */
-        list_head *selected_process = proc_fetch_or_alloc(process, pid);
-
-        while (fgets(buf, LINE_MAX, file))
-        {
-            if (buf[strlen(buf) - 1] == '\n')   /* replace fgets \n with \0 */
-                buf[strlen(buf) - 1] = '\0';
-            if ((ret_pid = process_fill_by_status(buf, selected_process, pid)) != 0)
-                read_proc_dir_by_pid(ret_pid, process);
+	if ((file = fopen(path, "r")) != NULL) {
+        list_head *selected_process = proc_fetch_or_alloc(list, pid);
+        if (child && child->process && child->process->pid) {
+            child->process->parent = selected_process->process;
+            child->process->parent->children = list_add_proc(child->process->parent->children, child->process);
         }
-        fclose(file);
-    }
+
+
+        PROC *proc = selected_process->process;
+		while (fgets(buf, LINE_MAX, file)) {
+			if (buf[strlen(buf) - 1] == '\n')	/* replace fgets \n with \0 */
+				buf[strlen(buf) - 1] = '\0';
+
+                if (str_starts_with(buf, "Name"))
+                    sscanf(buf, "%*s%s", proc->name);
+
+                if (str_starts_with(buf, "Pid"))
+                    sscanf(buf, "%*[^]0-9]%d", &proc->pid);
+
+                /* handle parent */
+                if (str_starts_with(buf, "PPid")) {
+                    sscanf(buf, "%*[^]0-9]%d", &ppid);
+                    if (ppid > 1)
+                        read_proc_dir_by_pid(ppid, selected_process, selected_process);
+                }
+
+
+		}
+		fclose(file);
+	}
 
 }
 
@@ -178,110 +274,81 @@ read_proc_dir_by_pid (pid_t pid, list_head *process)
  * @brief crawl PROC_BASE and create PROC entries
  */
 static void
-crawl_proc (list_head *list_head)
+crawl_proc(list_head * list_head)
 {
-    DIR *dirp;
-    struct dirent *de;
+	DIR *dirp;
+	struct dirent *de;
 
-    dirp = opendir(PROC_BASE);
-    if (dirp == NULL) {
-        errExit("opendir");
-    }
+	dirp = opendir(PROC_BASE);
+	if (dirp == NULL) {
+		errExit("opendir");
+	}
 
-    /* iterate over dirs in PROC_BASE */
-    for (;;) {
-        errno = 0;
-        pid_t pid;
+	/* iterate over dirs in PROC_BASE */
+	for (;;) {
+		errno = 0;
+		pid_t pid;
 
-        de = readdir(dirp);
-        if (de == NULL) /* EOS */
-            break;
+		de = readdir(dirp);
+		if (de == NULL)	/* EOS */
+			break;
 
-        pid = (pid_t) strtol(de->d_name, NULL, 10);
-        if (pid)
-            read_proc_dir_by_pid(pid, list_head);
-    }
+		pid = (pid_t) strtol(de->d_name, NULL, 10);
+		if (pid)
+			read_proc_dir_by_pid(pid, list_head, (void  *) NULL);
+	}
 
-    closedir(dirp);
+	closedir(dirp);
 
 }
 
-void
-print_list(list_head *list)
+
+
+int
+main(int argc, char *argv[])
 {
-    list_head *p;
-    klist_for_each(p, list) {
-        printf("Process: %zu - %s\n", (long) p->process->pid, p->process->name);
-    }
-}
+	/* create initial list element linked to itself */
+	list_head *list_head = list_init();
 
-list_head*
-list_init ()
-{
-    list_head *head = malloc(sizeof(*head));
-    if (head == NULL)
-        errExit("list_init alloc");
-
-    /* no process for initial element */
-
-    /*
-     * make this a circular list - makes traversing as simple as checking if 
-     * element == start element. thus traversing can start from any element.
-     */
-    head->next = head;
-
-    return head;
-}
-
-
-int main(int argc, char *argv[])
-{
-    /* create initial list element linked to itself */
-    list_head* list_head = list_init();
-
-    /* open current dir to return to later */
-    int ret;
+	/* open current dir to return to later */
+	int ret;
 
 #ifdef ROOT_RUN
-    int swd_fd;
-    swd_fd = open (".", O_RDONLY);
-    if (swd_fd == -1)
-        errExit("Could not open current dir\n");
+	int swd_fd;
+	swd_fd = open(".", O_RDONLY);
+	if (swd_fd == -1)
+		errExit("Could not open current dir\n");
 
-    /* change to root */
-    ret = chdir ("/");
-    if (ret == -1) {
-        errExit("Could not change to root dir\n");
-    }
+	/* change to root */
+	ret = chdir("/");
+	if (ret == -1) {
+		errExit("Could not change to root dir\n");
+	}
 #endif
 
-    crawl_proc(list_head);
-
-    print_list(list_head);
-
-    /*
-     * @TODO free_list(list_head);
-     */
+	/* create circular list of all unique running pids */
+	crawl_proc(list_head);
 
 
+	/* free_list(list_head); */
+	print_list(list_head);
 
 #ifdef ROOT_RUN
     /**
      * return to original dir. unneccessary as only process runs from the chdir.
      * here for example only.
      */
-    ret = fchdir (swd_fd);
-    if (ret == -1) {
-        errExit("Returning to original dir\n");
-    }
+	ret = fchdir(swd_fd);
+	if (ret == -1) {
+		errExit("Returning to original dir\n");
+	}
 
-    /* close dir */
-    ret = close (swd_fd);
-    if (ret == -1) {
-        errExit("Could not close original dir");
-    }
+	/* close dir */
+	ret = close(swd_fd);
+	if (ret == -1) {
+		errExit("Could not close original dir");
+	}
 #endif
 
-    
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
